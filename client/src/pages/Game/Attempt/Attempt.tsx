98d680305d2f70bd97df1a8b038cb2c4f3cf0395
TheMagicDragonMX <turbojet901@gmail.com>
2022-06-26 14:40:44 -0500

    Created Attempt component

---
+++ client/src/pages/Game/Attempt/Attempt.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./Attempt.scss"
+
+const Attempt = () => {
+  return (
+    <div className="attempt">
+      Attempt!
+    </div>
+  )
+}
+
+export default Attempt;
--- client/src/pages/Game/Game.tsx
+++ client/src/pages/Game/Game.tsx
@@ -1,10 +1,16 @@
 import React from "react";
+import Attempt from "./Attempt/Attempt";
 
 import "./Game.scss"
 
 const Game = () => {
   return (
     <div id="game">
+      <Attempt />
+      <Attempt />
+      <Attempt />
+      <Attempt />
+
       
     </div>
   )
