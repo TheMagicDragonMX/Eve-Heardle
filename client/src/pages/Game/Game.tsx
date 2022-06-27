7e60c0e8f43bfafc9731f36e482cbfe911483884
TheMagicDragonMX <turbojet901@gmail.com>
2022-06-27 10:58:33 -0500

    I hate SoundCloud >.<

--- client/src/pages/Game/Game.tsx
+++ client/src/pages/Game/Game.tsx
@@ -6,6 +6,15 @@ import SearchBar from "./SearchBar/SearchBar";
 import "./Game.scss"
 
 const Game = () => {
+
+  async function testSoundcloud () {
+    console.log("Fetching Soundcloud 🔊");
+    // Track ID: 290275679
+
+    const response = await fetch("")
+
+  }
+
   return (
     <div id="game">
       <ul id="list-of-attempts">
@@ -15,6 +24,10 @@ const Game = () => {
         <Attempt songName="Danger Close" isCorrect={ false } />
         <Attempt songName="Danger Close" isCorrect={ true } />
       </ul>
+
+      <div id="track-player">
+        <button onClick={ () => testSoundcloud() }>Retrieve track</button>
+      </div>
 
       <SearchBar />
 
